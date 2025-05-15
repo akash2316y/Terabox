669216246168df5b9c94e56c0dea76f6af2e30c2
akash2316y <iamakashrabha@gmail.com>
2025-05-15 12:23:00 +0530

    Update video.py

--- video.py
+++ video.py
@@ -59,7 +59,7 @@ def get_video_duration(file_path: str) -> int:
 
 # Terabox API Details
 TERABOX_API_URL = "https://terabox.web.id"
-TERABOX_API_TOKEN = "ffc819a2-31fa-4515-ab15-f0f71e1d63d2_6987158459"
+TERABOX_API_TOKEN = "bf0d7271-192f-468a-8a46-36179c113e3c_6987158459"
 THUMBNAIL = "https://envs.sh/JP6.jpg"
 db_channel_id = -1002536904769
 
