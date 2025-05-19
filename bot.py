00ba9243a9b9b3cac3d74364b0bdff7c1d82b946
akash2316y <iamakashrabha@gmail.com>
2025-05-19 10:48:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,1 @@
-# force subscribe main code :-
-from info import AUTH_CHANNEL
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.errors import *
 
-async def is_subscribed(bot, query, channel):
-    btn = []
-    for id in channel:
-        chat = await bot.get_chat(int(id))
-        try:
-            await bot.get_chat_member(id, query.from_user.id)
-        except UserNotParticipant:
-            btn.append([InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)])
-        except Exception as e:
-            pass
-    return btn
